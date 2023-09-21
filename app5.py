a5f905e74fdf309c0c2db82e86524aacbf6186aa
Praveen PRV <144040302+PRAVEEN1523@users.noreply.github.com>
2023-09-21 13:54:58 +0530

    Add files via upload

---
+++ app5.py
@@ -0,0 +1,12 @@
+a = 20;  #variable declration 
+b = 10; #variable declaration 
+#adding a and b using operator + 
+print("The value of adding the a and b is :", a+b) #print statement 
+#subracting a and b using operator -
+print("The value of subracting the a and b is :", a-b)
+#multiplying a and b using operator *
+print("The value of dividing the a and b is :", a*b)
+#dividing a and b using operator /
+print("The value of multiplying the a and b is :", a/b)
+#modulusing a and b using operator %
+print("The value of modulus the a and b is :", a%b)
